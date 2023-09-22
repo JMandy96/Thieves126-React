4d594d7820174f39a44da6398e91398bb9581f73
byaskew3 <byaskew3@gmail.com>
2023-09-22 11:54:49 -0600

    Stripe API Integration

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./views/Home";
 import PokemonData from "./views/PokemonData/PokemonData";
 import Team from "./views/Team";
+import { loadStripe, StripeElementsOptions } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import { useState, useEffect } from "react";
+import CheckoutForm from "./views/CheckoutForm";
+import CompletedPayment from "./views/CompletedPayment";
 
 const theme = createTheme({
     palette: {
@@ -17,7 +22,35 @@ const theme = createTheme({
     },
 });
 
+const stripePromise = loadStripe("pk_test_TYooMQauvdEDq54NiTphI7jx");
+
 const App = () => {
+    const [clientSecret, setClientSecret] = useState('')
+
+    useEffect(() => {
+        createPayment()
+      }, []);
+    
+    const createPayment = async () => {
+        const response = await fetch('http://localhost:5000/api/create_payment', {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ items: [{ id: "League of Legends" }, { id: "Call of Duty"}, { id: "Starfield"}] })
+        })
+        const data = await response.json()
+        console.log(data)
+        setClientSecret(data.clientSecret)
+    }
+
+
+    const options: StripeElementsOptions = {
+        clientSecret,
+        appearance: {
+            theme: 'stripe'
+        }
+    };
+
+
     return (
         <>
             <BrowserRouter>
@@ -28,6 +61,10 @@ const App = () => {
                         <Route path="/register" element={<Register/>} />
                         <Route path="/pokemonFinder" element={<PokemonData/>}/>
                         <Route path="/team" element={<Team/>}/>
+                        {clientSecret && (
+                            <Route path="/checkoutForm" element={<Elements options={options} stripe={stripePromise}><CheckoutForm/></Elements>}/>
+                        )}
+                        <Route path="/completedPayment" element={<CompletedPayment/>}/>
                     </Routes>
                 </ThemeProvider>
             </BrowserRouter>
---
+++ src/views/CompletedPayment.tsx
@@ -0,0 +1,6 @@
+const CompletedPayment = () => {
+  return (
+    <h1>CompletedPayment</h1>
+  )
+}
+export default CompletedPayment
